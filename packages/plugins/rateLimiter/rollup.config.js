135eb210f3ba8f526b3c33ed5139754358bbb6aa
PrashanthBeri <prashanthberi00@gmail.com>
2024-11-14 15:18:49 +0530

    rateLimiter release

--- packages/plugins/rateLimiter/rollup.config.js
+++ packages/plugins/rateLimiter/rollup.config.js
@@ -1,5 +1,9 @@
 // const { withNx } = require('@nx/rollup/with-nx');
 import { withNx } from '@nx/rollup/with-nx.js';
+import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default withNx(
   {
@@ -8,11 +12,17 @@ export default withNx(
     tsConfig: './tsconfig.lib.json',
     compiler: 'tsc',
     format: ['cjs', 'esm'],
-    assets: [{ input: '.', output: '.', glob: '*.md' }],
   },
   {
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
     // e.g.
     // output: { sourcemap: true },
+    output: {
+      exports: 'named',
+      interop: 'auto'
+    },
+    strictRequires: true,
+    external: ['graphql-rate-limit', 'graphql-middleware', 'graphql-shield', 'lodash', 'redis'],
+    plugins: [optimizeLodashImports(), commonjs(), typescript(), resolve()]
   }
 );
