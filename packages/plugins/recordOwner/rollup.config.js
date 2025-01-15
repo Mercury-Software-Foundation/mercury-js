74dd18728af2773defd12067f20a04ef5eb28525
PrashanthBeri <prashanthberi00@gmail.com>
2025-01-15 12:49:38 +0530

    recordOwner:rollup update for node apps

--- packages/plugins/recordOwner/rollup.config.js
+++ packages/plugins/recordOwner/rollup.config.js
@@ -1,11 +1,16 @@
-const { withNx } = require('@nx/rollup/with-nx');
 
-module.exports = withNx(
+import { withNx } from '@nx/rollup/with-nx.js';
+import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default withNx(
   {
     main: './src/index.ts',
     outputPath: '../../../dist/packages/plugins/recordOwner',
     tsConfig: './tsconfig.lib.json',
-    compiler: 'swc',
+    compiler: 'tsc',
     format: ['cjs', 'esm'],
     assets: [{ input: '.', output: '.', glob: '*.md' }],
   },
@@ -13,5 +18,12 @@ module.exports = withNx(
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
     // e.g.
     // output: { sourcemap: true },
+    // external: ['mongoose', 'lodash'],
+    output: {
+      exports: 'named',
+      interop: 'auto'
+    },
+    strictRequires: true,
+    plugins: [optimizeLodashImports(), commonjs(), typescript(), resolve()],
   }
 );
